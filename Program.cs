6bd7ccc4ef93ad455e3dada1ede23a886391d2d4
Abhishek Goswami <abhishek.gswmi11@gmail.com>
2023-06-23 00:37:54 +0530

    Add console log in CommandLine

--- Program.cs
+++ Program.cs
@@ -3,7 +3,8 @@ using System;
 namespace ProGit {
   class Program {
     static void main(string[] args) {
-      Console.writeLine("Hello World")
+      Console.writeLine("Hello World");
+      Console.WriteLine("Added this line in Command Line");
     }
   }
 }
