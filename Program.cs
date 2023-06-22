be54599fad830c0ea74aac6947bba5cf2abbc538
Abhishek Goswami <abhishek.gswmi11@gmail.com>
2023-06-23 00:41:38 +0530

    Update Program.cs in GithubDesktop

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System;
 namespace ProGit {
   class Program {
     static void main(string[] args) {
-      Console.writeLine("Hello World")
+      Console.WriteLine("Hello World");
     }
   }
 }
